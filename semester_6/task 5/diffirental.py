6a833ed6447df8439624356b7b6e2c987c90d08b
Илья <ilyasolonitsyn@mail.ru>
2022-05-13 14:42:24 +0300

    Класса с методами для расчётной схемы двухэтапного метода Рунге-Кутты для СОДУ с двумя уравнениями

--- semester_6/task 5/diffirental.py
+++ semester_6/task 5/diffirental.py
@@ -10,20 +10,33 @@ class CalculationScheme:
         self.b_1 = -5
         self.A = 1 / 12
         self.B = 1 / 20
-        self.x = 0
+        self.x_0 = 0
 
     def y_1(self, x):
-        if abs(x - self.x) < 1e-9:
+        h = abs(self.x_0 - x)
+        if h < 1e-9:
             return self.B * np.pi
-        # return self.y_1(x) + self.b_1*self.k11(h) +
+        return self.y_1(self.x_0) + self.b_1 * self.k_1(1, h) + self.b_2 * self.k_1(2, h)
 
     def y_2(self, x):
-        pass
+        h = abs(self.x_0 - x)
+        if h < 1e-9:
+            return self.A * np.pi
+        return self.y_2(self.x_0) + self.b_1 * self.k_2(1, h) + self.b_2 * self.k_2(2, h)
 
     def k_1(self, j, h):
         # j - воспринимать как k_1j, для удобства
-        pass
+        if j == 1:
+            return self.A * h * self.y_2(self.x_0)
+
+        return self.A * h * (self.y_2(self.x_0) + self.a_21 * self.k_2(1, h))
 
     def k_2(self, j, h):
-        # j - воспринимать как k_1j, для удобства
-        pass
+        # j - воспринимать как k_2j, для удобства
+        if j == 1:
+            return -self.B * h * self.y_1(self.x_0)
+
+        return -self.B * h * (self.y_1(self.x_0) + self.a_21 * self.k_1(1, h))
+
+
+scheme = CalculationScheme()
